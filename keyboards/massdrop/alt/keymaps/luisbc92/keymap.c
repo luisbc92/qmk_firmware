1cbaf82f00e324163c100c5aa9d4016535d7b7ed
Luis Bañuelos <luiscarlos.banuelos@gmail.com>
2021-07-13 22:01:42 -0700

    custom keymap

--- keyboards/massdrop/alt/keymaps/luisbc92/keymap.c
+++ keyboards/massdrop/alt/keymaps/luisbc92/keymap.c
@@ -16,7 +16,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, KC_HOME, \
         KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  KC_PGUP, \
         KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_PGDN, \
-        KC_LCTL, KC_LGUI, KC_LALT,                            KC_SPC,                             KC_RALT, MO(1),   KC_LEFT, KC_DOWN, KC_RGHT  \
+        KC_LCTL, KC_LALT, KC_LGUI,                            KC_SPC,                             MO(1), KC_RALT,   KC_LEFT, KC_DOWN, KC_RGHT  \
     ),
     [1] = LAYOUT_65_ansi_blocker(
         KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, KC_MUTE, \
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 // override rgb matrix
 void rgb_matrix_indicators_user(void) {
-    led_flags_t flags = rgb_matrix_get_flags(); 
+    led_flags_t flags = rgb_matrix_get_flags();
 
     if (HAS_FLAGS(flags, LED_FLAG_KEYLIGHT)) {
         uint8_t number_keys[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
@@ -56,7 +56,7 @@ void rgb_matrix_indicators_user(void) {
         for (int i = 0; i < 10; i++) {
             rgb_matrix_set_color(number_keys[i], 128, 255, 255);
         }
-        
+
         // left keys
         for (int i = 0; i < 6; i++) {
             rgb_matrix_set_color(left_keys[i], RGB_CYAN);
